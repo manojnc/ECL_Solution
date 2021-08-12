f1a8169d576f7e08fcc2bb1ca476089ca384f48d
root <root@ip-172-31-4-243.us-west-1.compute.internal>
2021-08-12 20:08:12 +0000

    final update

--- Solution.py
+++ Solution.py
@@ -15,51 +15,47 @@ class solution():
         self.heaparray=[]
 
     def parse_file(self):
-        if(not(os.path.isfile(self.filename))):
-            print('file not found')
-            os._exit(2)
         res={}
         line_count=0
 
         with open(self.filename,"r") as infile:
             for row in infile:
-                if row.rstrip():
+                if row.rstrip():                                            # skipping the empty lines 
                     try:
                         line_count+=1
-                        match=re.match(r'^(\d+):?(\s+)?(.*)?',row)
+                        match=re.match(r'^(\d+):?(\s+)?(.*)?',row)          # matching the pattern NUM : VAL
                         if match == None:
-                            print("Bad Input in the file")
+                            print("Unexpected content in the file")
                             os._exit(1)
                         score=int(match.group(1))
                         exp_json=json.loads(match.group(3))
 
                         if not(isinstance(exp_json,dict)):
                             os._exit(1)
-                        if exp_json.get('id',None) is None:
+                        if exp_json.get('id',None) is None:                 # validate for id key
                             os._exit(1)
 
                         uid=exp_json.get('id')
 
-                        if self.counter < self.num_records:
+                        if self.counter < self.num_records:                 # set up the heap array initially with N number of records requested
                             heap.heappush(self.heaparray,score)
-                            res[score]=uid
+                            res[score]=uid                                  # set up the result json onbject with score and associated id
                             self.counter+=1
                             continue
 
-                        if score > self.heaparray[0]:
-                            popped_ele=heap.heappop(self.heaparray)
-                            res.pop(popped_ele)
-                            heap.heappush(self.heaparray,score)
+                        if score > self.heaparray[0]:                       # if any subsequent elements are having higer score, replace the smallest element from heap with the new element
+                            popped_ele=heap.heappushpop(self.heaparray,score)
+                            res.pop(popped_ele)                             # remove the smaller scores and replace it with higher score key vale pair
                             res[score]=uid
                            
-                    except JSONDecodeError:
+                    except JSONDecodeError:                                 # validate if the value is not a valid json object 
                         print("bad json input in the file")
                         os._exit(1)
 
                     except Exception as e:
                         print(str(e))
                         continue
-        if self.num_records > line_count:
+        if self.num_records > line_count:                                   # number of records requested cannot exeed number of lines in the file
             sys.exit("Number of records requested is greater than total number of valid lines in the file")
         return res
 
